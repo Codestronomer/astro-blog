bb69fdef934f005901c503d4d9c8a7c84df08fda
byte.eon <Johnrumide6@gmail.com>
2025-02-19 23:59:12 +0100

    [feat] add hello button, theming and theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://byte-eon-blog.netlify.app"
+  site: "https://byte-eon-blog.netlify.app",
+  integrations: [preact()]
 });
